d2a6ba4837581d65a234f3f31054cca0a9d22d8b
zhangr011 <zhangr011@gmail.com>
2021-03-07 15:42:39 +0800

    warning area mark.

--- pyecharts_flask_kline/cboe_handlers.py
+++ pyecharts_flask_kline/cboe_handlers.py
@@ -24,14 +24,33 @@ def get_vix_info():
     rets_gvzm = gvzm.analyze()
     ovxm = OVXDataManager([])
     rets_ovxm = ovxm.analyze()
+    vix_diff = rets_vix['vix_diff'][[1]]
+    vix_diff.rename({1 : 'diff'}, axis = 1, inplace = True)
     df_gvz = rets_gvzm['gvz'][[CLOSE_PRICE_NAME]]
     df_gvz.rename({CLOSE_PRICE_NAME : 'gvz'}, axis = 1, inplace = True)
     df_ovx = rets_ovxm['ovx'][[CLOSE_PRICE_NAME]]
     df_ovx.rename({CLOSE_PRICE_NAME : 'ovx'}, axis = 1, inplace = True)
+    df = df.join(vix_diff)
     df = df.join(df_gvz)
     df = df.join(df_ovx)
     return df
 
+
+#----------------------------------------------------------------------
+def get_warning_areas(df):
+    """get the warning based on vix_diff"""
+    warning = False
+    areas = []
+    start = None
+    for date, diff in zip(df.index, df['diff']):
+        if warning is False and diff < -0.005:
+            warning = True
+            start = date
+        elif warning is True and diff >= 0.02:
+            warning = False
+            areas.append((start, date))
+            start = None
+    return areas
 
 #----------------------------------------------------------------------
 def line(delivery_dates, df):
@@ -39,6 +58,7 @@ def line(delivery_dates, df):
     FLINE_OPT = opts.LineStyleOpts(opacity = 1, width = 1.5)
     OLINE_OPT = opts.LineStyleOpts(opacity = 0.9, width = 1.2, type_ = 'dashed')
     df['delivery'] = [10 + 5 * (idx in delivery_dates) for idx in df.index]
+    warning_areas = get_warning_areas(df)
     line = (Line()
             .add_xaxis(xaxis_data = df.index)
             .add_yaxis("vix", df[0], is_symbol_show = False,
@@ -78,7 +98,12 @@ def line(delivery_dates, df):
             # add delivery date mark, not a good idea, but worked.
             .add_yaxis("delivery", df['delivery'], is_symbol_show = False, is_step = True)
             .set_series_opts(
-                label_opts = opts.LabelOpts(is_show = False))
+                label_opts = opts.LabelOpts(is_show = False),
+                markarea_opts = opts.MarkAreaOpts(
+                    is_silent = True,
+                    data = [opts.MarkAreaItem(name = "warn", x=(xs, xe)) for xs, xe in warning_areas],
+                    itemstyle_opts = opts.ItemStyleOpts(opacity = 0.2, color = '#FFA54F33')
+                ))
             .set_global_opts(
                 title_opts = opts.TitleOpts(title = "vix", pos_left = "0"),
                 xaxis_opts = opts.AxisOpts(
