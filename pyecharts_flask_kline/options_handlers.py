05ac036390d9ac01896fc5fce5acbcddda4129d2
zhangr011 <zhangr011@gmail.com>
2021-03-17 18:29:14 +0800

    siv chart update.

--- pyecharts_flask_kline/options_handlers.py
+++ pyecharts_flask_kline/options_handlers.py
@@ -52,12 +52,25 @@ def kline_chart(data: pd.DataFrame, product: str):
             series_name = "kline",
             y_axis = list(zip(data[CLOSE_PRICE_NAME], data[CLOSE_PRICE_NAME],
                               data[CLOSE_PRICE_NAME] * 0.995, data[CLOSE_PRICE_NAME] * 1.005)),
-            yaxis_index = 0,
             itemstyle_opts=opts.ItemStyleOpts(
                 color="#ef232a",
                 color0="#14b143",
                 border_color="#ef232a",
                 border_color0="#14b143",
+            ),
+        )
+        .extend_axis(
+            yaxis = opts.AxisOpts(
+                name = 'vix',
+                is_scale = True,
+                axislabel_opts = opts.LabelOpts(formatter = "{value} %"), interval = 5
+            )
+        )
+        .extend_axis(
+            yaxis = opts.AxisOpts(
+                is_show = False,
+                min_ = 0,
+                max_ = 500,
             ),
         )
         .set_series_opts(
@@ -84,21 +97,13 @@ def kline_chart(data: pd.DataFrame, product: str):
             ),
             yaxis_opts = opts.AxisOpts(
                 name = "指数价格",
-                position = "right",
+                position = "left",
                 is_scale = True,
                 splitline_opts = opts.SplitLineOpts(is_show = True),
             ),
             tooltip_opts = opts.TooltipOpts(trigger = "axis", axis_pointer_type = "line"),
-            legend_opts = opts.LegendOpts(is_show = False),
-            datazoom_opts=[
-                opts.DataZoomOpts(
-                    is_show=False, type_="inside", xaxis_index=[0, 0], range_start = 0, range_end=100
-                ),
-                opts.DataZoomOpts(
-                    is_show=True, xaxis_index=[0, 1], pos_top="97%", range_start = 0, range_end=100
-                ),
-                opts.DataZoomOpts(is_show=False, xaxis_index=[0, 2], range_start = 0, range_end=100),
-            ],
+            legend_opts = opts.LegendOpts(is_show = True),
+            datazoom_opts = opts.DataZoomOpts(type_ = "slider", range_start = 0, range_end = 100),
         )
     )
 
@@ -122,7 +127,7 @@ def kline_chart(data: pd.DataFrame, product: str):
         .add_yaxis(
             series_name = "hv20",
             y_axis = data[HV_20_NAME] * 100,
-            yaxis_index = 2,
+            yaxis_index = 1,
             # is_smooth=True,
             linestyle_opts = opts.LineStyleOpts(opacity = 0.9, width = 1.2),
             label_opts = opts.LabelOpts(is_show = False),
@@ -130,36 +135,15 @@ def kline_chart(data: pd.DataFrame, product: str):
         .add_yaxis(
             series_name = "hv250",
             y_axis = data[HV_250_NAME] * 100,
-            yaxis_index = 3,
+            yaxis_index = 1,
             # is_smooth=True,
             linestyle_opts = opts.LineStyleOpts(opacity = 0.8, width = 1),
             label_opts = opts.LabelOpts(is_show = False),
         )
-        .set_global_opts(
-            xaxis_opts=opts.AxisOpts(
-                type_="category",
-                grid_index=1,
-                axislabel_opts = opts.LabelOpts(is_show = False),
-            ),
-            yaxis_opts=opts.AxisOpts(
-                grid_index = 1,
-                split_number = 4,
-                # position = "right",
-                axisline_opts=opts.AxisLineOpts(is_on_zero=False),
-                axistick_opts=opts.AxisTickOpts(is_show=False),
-                axislabel_opts=opts.LabelOpts(is_show=True),
-                splitline_opts = opts.SplitLineOpts(is_show = False),
-            ),
-        )
-    )
-
-    ivp_line = (
-        Line(init_opts = opts.InitOpts())
-        .add_xaxis(xaxis_data = dates)
         .add_yaxis(
             series_name = "ivp",
             y_axis = data[IV_PER],
-            yaxis_index = 1,
+            yaxis_index = 2,
             # is_smooth=True,
             linestyle_opts = opts.LineStyleOpts(
                 opacity = 1,
@@ -178,70 +162,11 @@ def kline_chart(data: pd.DataFrame, product: str):
                          ]}),
             label_opts = opts.LabelOpts(is_show = False),
         )
-        .set_global_opts(
-            xaxis_opts=opts.AxisOpts(
-                type_="category",
-                grid_index=1,
-                axislabel_opts = opts.LabelOpts(is_show = False),
-            ),
-            yaxis_opts=opts.AxisOpts(
-                grid_index = 1,
-                split_number = 4,
-                # position = "right",
-                axisline_opts=opts.AxisLineOpts(is_on_zero=False),
-                axistick_opts=opts.AxisTickOpts(is_show=False),
-                axislabel_opts=opts.LabelOpts(is_show=True),
-                splitline_opts = opts.SplitLineOpts(is_show = False),
-            ),
-            legend_opts = opts.LegendOpts(is_show = False),
-        )
-    )
-
-    # options' volume / futures' open interest
-    oi = []
-    try:
-        oi = data[OPEN_INTEREST_NAME]
-    except KeyError:
-        pass
-    bar_1 = (
-        Bar(init_opts = opts.InitOpts())
-        .add_xaxis(xaxis_data = data.index)
-        .add_yaxis(
-            series_name = "futures oi",
-            y_axis = oi,
-            xaxis_index = 1,
-            yaxis_index = 1,
-            label_opts = opts.LabelOpts(is_show = False),
-        )
-        .set_global_opts(
-            xaxis_opts=opts.AxisOpts(
-                type_="category",
-                grid_index=2,
-                axislabel_opts=opts.LabelOpts(is_show=False),
-            ),
-            legend_opts=opts.LegendOpts(is_show=False),
-        )
     )
 
     # K线图和 siv 图
-    pos_left_str = "3%"
-    pos_right_str = "5%"
-    grid_chart.add(
-        kline,
-        grid_opts = opts.GridOpts(pos_left = pos_left_str, pos_right = pos_right_str, height = "70%"),
-    ).add(
-        siv_line,
-        grid_opts = opts.GridOpts(pos_left = pos_left_str, pos_right = pos_right_str, height = "70%"),
-    ).add(
-        ivp_line,
-        grid_opts = opts.GridOpts(pos_left = pos_left_str, pos_right = pos_right_str, pos_bottom = "3%", height = "15%"),
-    ).add(
-        bar_1,
-        grid_opts = opts.GridOpts(
-            pos_left = pos_left_str, pos_right = pos_right_str, pos_bottom = "3%", height = "15%"
-        ),
-    )
-    return grid_chart
+    kline.overlap(siv_line)
+    return kline
 
 
 #----------------------------------------------------------------------
