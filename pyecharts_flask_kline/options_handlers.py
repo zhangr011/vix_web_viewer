0d63235e95f590862ffdc682900566facd2ef3b4
zhangr011 <zhangr011@gmail.com>
2021-04-06 23:42:32 +0800

    update the kline and the colors

--- pyecharts_flask_kline/options_handlers.py
+++ pyecharts_flask_kline/options_handlers.py
@@ -45,19 +45,114 @@ def kline_chart(data: pd.DataFrame, product: str):
     grid_chart = Grid(init_opts = opts.InitOpts(theme = THEME_ME))
     dates = data.index.to_list()
 
+    colors = ['ivory', 'crimson', 'gold', 'cyan', 'teal', 'tan', 'cyan', 'red']
+
+    # two lines to show ivp, normal ivp with cyan, warn vip for red.
+    ivp = data[IV_PER]
+    ivp_shift_left = ivp.shift(-1)
+    data['ivp_warn'] = data[IV_PER][(ivp >= 91) | (ivp <= 15) |
+                                    (ivp_shift_left >= 91) | (ivp_shift_left <= 15)]
+
+    hv_show = True
+    if not data[IV_NAME].isnull().all():
+        hv_show = False
+
     kline = (
-        Kline(init_opts = opts.InitOpts())
+        Line(init_opts = opts.InitOpts())
         .add_xaxis(xaxis_data = dates)
         .add_yaxis(
             series_name = "kline",
-            y_axis = list(zip(data[CLOSE_PRICE_NAME], data[CLOSE_PRICE_NAME],
-                              data[CLOSE_PRICE_NAME] * 0.995, data[CLOSE_PRICE_NAME] * 1.005)),
-            itemstyle_opts=opts.ItemStyleOpts(
-                color="#ef232a",
-                color0="#14b143",
-                border_color="#ef232a",
-                border_color0="#14b143",
+            y_axis = data[CLOSE_PRICE_NAME],
+            color = colors[7],
+            linestyle_opts = opts.LineStyleOpts(opacity = 1, width = 2.),
+            markline_opts = opts.MarkLineOpts(
+                data = [
+                    opts.MarkLineItem(type_ = "min", name = "最低价", symbol = 'none'),
+                    opts.MarkLineItem(type_ = "max", name = "最高价", symbol = 'none'),
+                ]
             ),
+        )
+        .add_yaxis(
+            series_name = "siv",
+            y_axis = data[IV_NAME] * 100,
+            yaxis_index = 1,
+            is_symbol_show = False,
+            # is_smooth=True,
+            color = colors[6],
+            markline_opts = opts.MarkLineOpts(
+                data = [
+                    opts.MarkLineItem(type_ = "min", name = "ivl"),
+                    opts.MarkLineItem(type_ = "max", name = "ivh"),
+                ]
+            ),
+            linestyle_opts = opts.LineStyleOpts(opacity = 1, width = 1.5),
+            label_opts = opts.LabelOpts(is_show = False),
+        )
+        .add_yaxis(
+            series_name = "siv5",
+            y_axis = data[IV_NAME].rolling(5).mean() * 100,
+            yaxis_index = 1,
+            is_symbol_show = False,
+            # is_smooth=True,
+            color = colors[5],
+            linestyle_opts = opts.LineStyleOpts(opacity = 0.9, width = 1.2),
+            label_opts = opts.LabelOpts(is_show = False),
+        )
+        .add_yaxis(
+            series_name = "siv10",
+            y_axis = data[IV_NAME].rolling(10).mean() * 100,
+            yaxis_index = 1,
+            is_symbol_show = False,
+            # is_smooth=True,
+            color = colors[4],
+            linestyle_opts = opts.LineStyleOpts(opacity = 0.9, width = 1.2),
+            label_opts = opts.LabelOpts(is_show = False),
+        )
+        .add_yaxis(
+            series_name = "hv20",
+            y_axis = data[HV_20_NAME] * 100,
+            yaxis_index = 1,
+            is_symbol_show = False,
+            is_selected = hv_show,
+            # is_smooth=True,
+            color = colors[3],
+            linestyle_opts = opts.LineStyleOpts(opacity = 0.9, width = 1.2),
+            label_opts = opts.LabelOpts(is_show = False),
+        )
+        .add_yaxis(
+            series_name = "hv250",
+            y_axis = data[HV_250_NAME] * 100,
+            yaxis_index = 1,
+            is_symbol_show = False,
+            is_selected = hv_show,
+            # is_smooth=True,
+            color = colors[2],
+            linestyle_opts = opts.LineStyleOpts(opacity = 0.8, width = 1),
+            label_opts = opts.LabelOpts(is_show = False),
+        )
+        .add_yaxis(
+            series_name = "ivp",
+            y_axis = ivp,
+            yaxis_index = 2,
+            is_symbol_show = False,
+            # is_smooth=True,
+            color = colors[1],
+            linestyle_opts = opts.LineStyleOpts(
+                opacity = 1,
+                width = 1.2),
+            label_opts = opts.LabelOpts(is_show = False),
+        )
+        .add_yaxis(
+            series_name = "ivp_warn",
+            y_axis = data['ivp_warn'],
+            yaxis_index = 2,
+            is_symbol_show = False,
+            color = colors[0],
+            linestyle_opts = opts.LineStyleOpts(
+                opacity = 1,
+                width = 1.2),
+            # is_smooth=True,
+            label_opts = opts.LabelOpts(is_show = False),
         )
         .extend_axis(
             yaxis = opts.AxisOpts(
@@ -77,12 +172,6 @@ def kline_chart(data: pd.DataFrame, product: str):
         .set_series_opts(
             label_opts = opts.LabelOpts(is_show=False),
             # 'circle', 'rect', 'roundRect', 'triangle', 'diamond', 'pin', 'arrow', 'none'
-            markline_opts = opts.MarkLineOpts(
-                data = [
-                    opts.MarkLineItem(type_ = "min", name = "最低价", symbol = 'none'),
-                    opts.MarkLineItem(type_ = "max", name = "最高价", symbol = 'none'),
-                ]
-            ),
         )
         .set_global_opts(
             title_opts = opts.TitleOpts(title = f"{product} siv", pos_left="0"),
@@ -108,100 +197,6 @@ def kline_chart(data: pd.DataFrame, product: str):
         )
     )
 
-    # two lines to show ivp, normal ivp with cyan, warn vip for red.
-    ivp = data[IV_PER]
-    ivp_shift_left = ivp.shift(-1)
-    data['ivp_warn'] = data[IV_PER][(ivp >= 91) | (ivp <= 15) |
-                                    (ivp_shift_left >= 91) | (ivp_shift_left <= 15)]
-
-    hv_show = True
-    if not data[IV_NAME].isnull().all():
-        hv_show = False
-
-    siv_line = (
-        Line(init_opts = opts.InitOpts())
-        .add_xaxis(xaxis_data = dates)
-        .add_yaxis(
-            series_name = "siv",
-            y_axis = data[IV_NAME] * 100,
-            yaxis_index = 1,
-            is_symbol_show = False,
-            # is_smooth=True,
-            markline_opts = opts.MarkLineOpts(
-                data = [
-                    opts.MarkLineItem(type_ = "min", name = "ivl"),
-                    opts.MarkLineItem(type_ = "max", name = "ivh"),
-                ]
-            ),
-            linestyle_opts = opts.LineStyleOpts(opacity = 1, width = 1.5),
-            label_opts = opts.LabelOpts(is_show = False),
-        )
-        .add_yaxis(
-            series_name = "siv5",
-            y_axis = data[IV_NAME].rolling(5).mean() * 100,
-            yaxis_index = 1,
-            is_symbol_show = False,
-            # is_smooth=True,
-            linestyle_opts = opts.LineStyleOpts(opacity = 0.9, width = 1.2, color = 'gold'),
-            label_opts = opts.LabelOpts(is_show = False),
-        )
-        .add_yaxis(
-            series_name = "siv10",
-            y_axis = data[IV_NAME].rolling(10).mean() * 100,
-            yaxis_index = 1,
-            is_symbol_show = False,
-            # is_smooth=True,
-            linestyle_opts = opts.LineStyleOpts(opacity = 0.9, width = 1.2, color = 'cyan'),
-            label_opts = opts.LabelOpts(is_show = False),
-        )
-        .add_yaxis(
-            series_name = "hv20",
-            y_axis = data[HV_20_NAME] * 100,
-            yaxis_index = 1,
-            is_symbol_show = False,
-            is_selected = hv_show,
-            # is_smooth=True,
-            linestyle_opts = opts.LineStyleOpts(opacity = 0.9, width = 1.2),
-            label_opts = opts.LabelOpts(is_show = False),
-        )
-        .add_yaxis(
-            series_name = "hv250",
-            y_axis = data[HV_250_NAME] * 100,
-            yaxis_index = 1,
-            is_symbol_show = False,
-            is_selected = hv_show,
-            # is_smooth=True,
-            linestyle_opts = opts.LineStyleOpts(opacity = 0.8, width = 1),
-            label_opts = opts.LabelOpts(is_show = False),
-        )
-        .add_yaxis(
-            series_name = "ivp",
-            y_axis = ivp,
-            yaxis_index = 2,
-            is_symbol_show = False,
-            # is_smooth=True,
-            linestyle_opts = opts.LineStyleOpts(
-                opacity = 1,
-                width = 1.2,
-                color = 'cyan'),
-            label_opts = opts.LabelOpts(is_show = False),
-        )
-        .add_yaxis(
-            series_name = "ivp_warn",
-            y_axis = data['ivp_warn'],
-            yaxis_index = 2,
-            is_symbol_show = False,
-            linestyle_opts = opts.LineStyleOpts(
-                opacity = 1,
-                width = 1.2,
-                color = 'red'),
-            # is_smooth=True,
-            label_opts = opts.LabelOpts(is_show = False),
-        )
-    )
-
-    # K线图和 siv 图
-    kline.overlap(siv_line)
     return kline
 
 
